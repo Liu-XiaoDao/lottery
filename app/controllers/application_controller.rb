b7973ddaf097444b98e8b614a6619066d9393627
liuchunliang <Chunliang.Liu@abcam.com>
2018-12-27 19:34:30 +0800

    记录访问历史时排除photo和file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def process_action *args
     time = Time.now
     rslt = super
-    filtered_params = request.params.reject{|k, v| k.match('file') || v.empty?}
+    filtered_params = request.params.reject{|k, v| k.match('photo') || k.match('file') || v.empty?}
     ur=UserRequest.create(
       :url => request.url,
       :time => (Time.now-time),
