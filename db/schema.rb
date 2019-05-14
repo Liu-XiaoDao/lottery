c2e14b44f40b57ce2d6d2588fad9b2c7391cf0e1
liuchunliang <Chunliang.Liu@abcam.com>
2019-05-14 13:06:35 +0800

    删除注册后显示头像的地方

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190514043209) do
+ActiveRecord::Schema.define(version: 20190514050109) do
 
   create_table "families", force: :cascade do |t|
     t.string "name"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 20190514043209) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "attendance"
-    t.string "avatar_url"
     t.boolean "is_active", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
