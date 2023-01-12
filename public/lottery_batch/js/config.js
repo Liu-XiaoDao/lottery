8c522fd997663caac018cef07cd8b35407507bf0
clliu <chunliang.liu@abcam.com>
2023-01-12 22:22:24 +0800

    2023

--- public/lottery_batch/js/config.js
+++ public/lottery_batch/js/config.js
@@ -1,30 +1,30 @@
-var levelXF = 45,	//幸福奖 300
-	level3 = 16,		//三等奖 6
-	level2 = 7,		//二等奖 3
+var levelXF = 27,	//幸福奖 300
+	level3 = 9,		//三等奖 6
+	level2 = 3,		//二等奖 3
 	level1 = 1;		//一等奖 2
 
 //奖项类别
 window.prizeLevel = [{
 		'label': 'levelXF',
 		'text': '四等奖',
-		'count': 45, //总个数
+		'count': 27, //总个数
 		'complete': false,
-		'surplus': 45,	//剩余个数
-		'n': 3	//一次抽取个数
+		'surplus': 27,	//剩余个数
+		'n': 2	//一次抽取个数
 	},{
 		'label': 'level3',
 		'text': '三等奖',
 		'count': level3,
 		'complete': false,
 		'surplus': level3,
-		'n': 4
+		'n': 1
 	},{
 		'label': 'level2',
 		'text': '二等奖',
 		'count': level2,
 		'complete': false,
 		'surplus': level2,
-		'n': 4
+		'n': 1
 	},{
 		'label': 'level1',
 		'text': '一等奖',
