3e32c3f38efd733330611d4ed56f42222fbb3ebd
clliu <chunliang.liu@abcam.com>
2024-09-22 21:23:37 +0800

    使用activity获取时间

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,17 @@
 class Activity < ApplicationRecord
+  def self.title
+    last.name
+  end
+
+  def self.desc
+    last.desc
+  end
+
+  def self.time
+    last.date
+  end
+
+  def self.address
+    last.address
+  end
 end
